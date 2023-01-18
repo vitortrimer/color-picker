96f3a5b630c3c72835e400f5f405f7995a71b989
Vitor Trimer <vitortrimer@gmail.com>
2023-01-17 21:21:25 -0300

    Initial Commit

---
+++ Colorpicker/ColorpickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorpickerApp.swift
+//  Colorpicker
+//
+//  Created by Vitor Trimer on 17/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorpickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
