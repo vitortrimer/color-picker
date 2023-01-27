f2fe7b5bfb4db632dfc56b9c3409f4f1dd9bb293
Vitor Trimer <vitortrimer@gmail.com>
2023-01-26 23:29:38 -0300

    done

--- Colorpicker/View/Home.swift
+++ Colorpicker/View/Home.swift
@@ -12,6 +12,7 @@ struct Home: View {
     @State var currentItem: ColorValue?
     @State var expandCard: Bool = false
     @State var moveCardDown: Bool = false
+    @State var animateContent: Bool = false
     
     @Namespace var animation
     
@@ -66,8 +67,85 @@ struct Home: View {
         ColorView(item: item, rectSize: rectSize)
             .ignoresSafeArea()
             .overlay(alignment: .top) {
-                
+                ColorDetails(item: item, rectSize: rectSize)
             }
+            .overlay(content: {
+                DetailViewContent(item: item)
+            })
+    }
+    
+    @ViewBuilder
+    func DetailViewContent(item: ColorValue) -> some View {
+        VStack(spacing: 0) {
+            Rectangle()
+                .fill(.white)
+                .frame(height: 1)
+                .scaleEffect(x: animateContent ? 1 : 0.001, anchor: .leading)
+                .padding(.top, 60)
+            
+            VStack(spacing: 30) {
+                let rgbColor = NSColor(item.color).rgb
+                CustomProgressView(value: rgbColor.red, label: "Red")
+                CustomProgressView(value: rgbColor.green, label: "Green")
+                CustomProgressView(value: rgbColor.blue, label: "Blue")
+            }
+            .padding(15)
+            .background {
+                RoundedRectangle(cornerRadius: 15, style: .continuous)
+                    .fill(.ultraThinMaterial)
+                    .environment(\.colorScheme, .dark)
+            }
+            .opacity(animateContent ? 1 : 0)
+            .offset(y: animateContent ? 0 : 100)
+            .animation(.easeInOut(duration: 0.3).delay(animateContent ? 0.2 : 0), value: animateContent)
+            .padding(.top, 30)
+            .padding(.horizontal, 20)
+        }
+        .padding(.horizontal, 15)
+        .frame(maxHeight: .infinity, alignment: .top)
+        
+        HStack(spacing: 15) {
+            Text("Copy Code")
+                .fontWeight(.semibold)
+                .padding(.vertical, 8)
+                .padding(.horizontal, 30)
+                .background {
+                    Capsule()
+                        .fill(.white)
+                }
+                .onTapGesture {
+                    NSPasteboard.general.setString("#\(item.colorCode)", forType: .string)
+                }
+            Text("Dismiss")
+                .fontWeight(.semibold)
+                .padding(.vertical, 8)
+                .padding(.horizontal, 30)
+                .background {
+                    Capsule()
+                        .fill(.white)
+                }
+                .onTapGesture {
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        animateContent = false
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                        withAnimation(.easeInOut(duration: 0.3)) {
+                            expandCard = false
+                            moveCardDown = false
+                        }
+                    }
+                }
+        }
+        .opacity(animateContent ? 1 : 0)
+        .offset(y: animateContent ? 0 : 150)
+        .animation(.easeInOut(duration: 0.3).delay(animateContent ? 0.2 : 0), value: animateContent)
+        .padding(.horizontal, 16)
+        .onAppear() {
+            withAnimation(.easeInOut.delay(0.2)) {
+                animateContent = true
+            }
+        }
     }
     
     @ViewBuilder
@@ -77,7 +155,7 @@ struct Home: View {
         if !(item.id == currentItem?.id && expandCard) {
             ColorView(item: item, rectSize: rectSize)
                 .overlay(content: {
-                    ColorDetails(item: item)
+                    ColorDetails(item: item, rectSize: rectSize)
                 })
                 .frame(height: 110)
                 .contentShape(Rectangle())
@@ -85,12 +163,12 @@ struct Home: View {
                 .zIndex(tappedCard ? 100 : 0)
                 .onTapGesture {
                     currentItem = item
-                    withAnimation(.interactiveSpring(response: 0.3, dampingFraction: 0.4, blendDuration: 0.4)) {
+                    withAnimation(.interactiveSpring(response: 0.3, dampingFraction: 0.4, blendDuration: 0.3)) {
                         moveCardDown = true
                     }
                     
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.22) {
-                        withAnimation(.interactiveSpring(response: 0.4, dampingFraction: 1, blendDuration: 1)) {
+                        withAnimation(.interactiveSpring(response: 0.4, dampingFraction: 1, blendDuration: 0.3)) {
                             expandCard = true
                         }
                     }
@@ -123,7 +201,7 @@ struct Home: View {
     }
     
     @ViewBuilder
-    func ColorDetails(item: ColorValue) -> some View {
+    func ColorDetails(item: ColorValue, rectSize: CGSize) -> some View {
         HStack {
             Text("#\(item.colorCode)")
                 .font(.title.bold())
@@ -140,13 +218,54 @@ struct Home: View {
                     .font(.caption)
                     .foregroundColor(.white)
             }
+            .frame(width: rectSize.width * 0.3, alignment: .leading)
         }
         .padding([.leading, .vertical, .trailing], 16)
+        .matchedGeometryEffect(id: item.id.uuidString + "DETAILS", in: animation)
+    }
+    
+    @ViewBuilder
+    func CustomProgressView(value: CGFloat, label: String) -> some View {
+        HStack(spacing: 15) {
+            Text(label)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .frame(width: 40, alignment: .leading)
+            
+            GeometryReader {
+                let size = $0.size
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .fill(.black.opacity(0.75))
+                    
+                    Rectangle()
+                        .fill(.white)
+                        .frame(width: animateContent ? size.width * value : 0)
+                }
+            }
+            .frame(height: 8)
+            Text("\(Int(value * 255.0))")
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+        }
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension NSColor {
+    var rgb: (red: CGFloat, green: CGFloat, blue: CGFloat) {
+        let colorSpace = usingColorSpace(.extendedSRGB) ?? .init(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        colorSpace.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        return (red, green, blue)
     }
 }
